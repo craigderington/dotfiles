4ffee711b86ede55dfaaf1ee0aa03527da816406
Craig Derington <craig@craigderington.dev>
2025-08-13 15:35:53 -0400

    fix hyprpaper config and socket error.

---
+++ hyprland/.config/hypr/change_wallpaper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#   _   _                                              
+#  | | | |_   _ _ __  _ __ _ __   __ _ _ __   ___ _ __ 
+#  | |_| | | | | '_ \| '__| '_ \ / _` | '_ \ / _ \ '__|
+#  |  _  | |_| | |_) | |  | |_) | (_| | |_) |  __/ |   
+#  |_| |_|\__, | .__/|_|  | .__/ \__,_| .__/ \___|_|   
+#         |___/|_|        |_|         |_|              
+#
+#
+WALLPAPER_DIRECTORY="/home/craigderington/.config/backgrounds/"
+WALLPAPER=$(find $WALLPAPER_DIRECTORY -type f | shuf -n 1)
+
+#echo $WALLPAPER_DIRECTORY
+#echo $WALLPAPER
+
+hyprctl hyprpaper preload $WALLPAPER
+hyprctl hyprpaper wallpaper "DP-1,$WALLPAPER"
+
+sleep 0.10
+
+hyprctl hyprpaper unload unused
